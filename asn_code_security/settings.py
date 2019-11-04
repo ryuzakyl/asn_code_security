9a7f9323146efbb6b95ae5bd2a2934434334cfd1
L <ryuzakyl@gmail.com>
2019-11-04 19:29:34 +0100

    allow requests from anywhere in settings file

--- asn_code_security/settings.py
+++ asn_code_security/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jo_zs-)+_rzgx3b3778@0vafs&r%6vm+&vp^s-d1@o^w4puos^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
