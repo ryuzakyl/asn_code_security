b91e75fec91446ce8918d9d200d5c63245afe83b
L <ryuzakyl@gmail.com>
2019-11-04 16:33:55 +0100

    change implementation to use templates

--- poc/views.py
+++ poc/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 
 def example1(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'poc/example1.html')
